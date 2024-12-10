7335818e07c66fb43a6a2138352fbaf17af01fe9
Dev-Tech225 <devbhakta642@gmail.com>
2024-12-10 13:24:33 -0700

    modified:   graphics.py

--- graphics.py
+++ graphics.py
@@ -1,11 +1,16 @@
 from common import *
+from particle import _MetaParticle
 
 canvasSizeX = 800
 canvasSizeY = 800
+particleSize = 5
 #aspectRatio = (canvasSizeX/canvasSizeY)
 
 def renderBackground():
-    pass
-
+    Rect(0,0,canvasSizeX,canvasSizeY,fill='black')
+    Line(0,375,400,375,fill='white')
 def renderGame():
     pass
+
+def renderParticle(particleToRender: _MetaParticle):
+    Rect(particleToRender.pos.x, particleToRender.pos.y, particleToRender.pos.x+particleSize, particleToRender.pos.y+particleSize, fill=particleToRender.color)
