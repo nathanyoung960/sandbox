62fa2632621c3d11db46e01e13874bd13a10b328
Dev-Tech225 <devbhakta642@gmail.com>
2024-12-12 13:29:53 -0700

    modified:   cmu_graphics/meta/updates.json 	modified:   graphics.py

--- graphics.py
+++ graphics.py
@@ -6,15 +6,16 @@ canvasSizeX = 800
 canvasSizeY = 800
 particleSize = 5
 #aspectRatio = (canvasSizeX/canvasSizeY)
-
+color=['magenta','red']
 def renderBackground(app):
-    drawRect(0, 0, canvasSizeX, canvasSizeY, fill="black")
+    drawRect(0, 0, canvasSizeX, canvasSizeY, fill=color)
     drawLine(0,700,700,700,fill='white')
     drawLine(700,0,700,700,fill='white')
     drawLabel('Walls',750,300,fill='white',size=20)
     drawLabel('Explosives',750,350,fill='white',size=20)
     drawLabel('Powders',750,400,fill='white',size=20)
-
+    drawLabel('Gases',750,450,fill='white',size=20)
+    drawRect(50,725,50,50,fill='grey',visible=False)
 physicsSingleton = Physics.__singleton__()
 def renderGame(app):
     for p in physicsSingleton.grid.tileArray:
