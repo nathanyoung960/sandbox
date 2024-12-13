10973a8ec18c8d0415fd7d0024d95deef0b5f939
Dev-Tech225 <devbhakta642@gmail.com>
2024-12-13 14:11:12 -0700

    modified:   cmu_graphics/meta/updates.json

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ from particle import *
 physicsHandler = Physics.__singleton__()
 
 def onAppStart(app):
+    app.drawWall1 = False
+    app.drawLiquid1=False
     for x in range(6):
         for y in range(6):
             physicsHandler.addParticleToGrid(Sand(Vector2(x, y*2)))
-    # physicsHandler.addParticleToGrid(Sand(Vector2(50, 60)))
-    # physicsHandler.addParticleToGrid(Sand(Vector2(50, 50)))
-    # physicsHandler.addParticleToGrid(Sand(Vector2(50, 40)))
 
 def redrawAll(app):
     graphics.renderBackground(app)
@@ -22,6 +21,17 @@ def redrawAll(app):
 def onStep(app):
     physicsHandler.tickAllParticles()
 
+def onMousePress(app, mouseX, mouseY):
+    if (mouseX >= 720) and (mouseX <= 790) and (mouseY >= 250) and (mouseY <= 320):
+        print("Walls")
+        app.drawWall1=True
+    else:
+        app.drawWall1=False
+    if (mouseX >= 720) and (mouseX <= 790) and (mouseY >= 350) and (mouseY <= 420):
+        print("Liquids")
+        app.drawLiquid1=True
+    else:
+        app.drawLiquid1=False
 # app.width = graphics.canvasSizeX
 # app.height = graphics.canvasSizeY
 runApp(width = graphics.canvasSizeX, height = graphics.canvasSizeY)
